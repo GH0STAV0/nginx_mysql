cbc96c8ed2d0689d95c8cf79762eedda253216e1
cata0nana <101589752+cata0nana@users.noreply.github.com>
2022-10-26 20:37:14 +0000

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "us-cdbr-iron-east-02.cleardb.net",
-  USER: "b7e24378878xxx",
-  PASSWORD: "0200exxx",
-  DB: "heroku_7643ec736354xxx"
+  HOST: "mysql-52oq:33060",
+  USER: "mysql",
+  PASSWORD: "w2XQ5PlPFXc8juUkFXtEVQcg0sSP5mlA",
+  DB: "mysql"
 };
